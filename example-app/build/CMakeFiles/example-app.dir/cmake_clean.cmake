039af194295b382a6fe7c18a7e25a4dc8f6ab72a
deisler134 <microgalaxy134@gmail.com>
2019-04-26 10:09:54 -0400

    add test

---
+++ example-app/build/CMakeFiles/example-app.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example-app.dir/home/deisler/Downloads/resource/project/workspace/pytorch_C++/example-app/example-app.cpp.o"
+  "example-app.pdb"
+  "example-app"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example-app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
